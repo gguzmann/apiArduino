459e67b92dcc4f54548894dec0b9f3dc125bb5d4
gabusho92 <g.guzman.nunez@gmail.com>
2022-06-05 23:34:47 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,35 @@ const router = express.Router();
 
 
 let count = 0;
+let temp = 0;
+let hume = 0;
+let hora = 0;
 
 router.get('/', (req, res) => {
     console.log('Solicitud get');
     count++;
     res.json({
-        ok: count
+        ok: count,
+        temperatura: temp,
+        humedad: hume,
+        date: hora
     })
 })
 
 router.post('/', (req, res) => {
     const {temperatura} = req.body;
+    let {humerdad} = req.body;
+    // humedad = 1;
+    temp = temperatura;
+    hume = humerdad;
     
     let date = new Date()
     let min = date.getMinutes();
     let hour = date.getHours();
     let dateNow = `${hour}:${min}`
+    hora = dateNow;
 
-
-    console.log("Solicitud post. temperatura:", temperatura, dateNow)
+    console.log("Solicitud post. temperatura:", temperatura, humedad, dateNow)
     res.json({
         ok: 'post',
         date: dateNow
