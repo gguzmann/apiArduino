4e9a197ccb0e4fa2e3de6156ef833c072fd8b508
gabusho92 <g.guzman.nunez@gmail.com>
2022-06-13 10:36:52 -0400

    change api

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,61 @@ const router = express.Router();
 
 
 let count = 0;
-let listaArduinos = [];
+const listaArduinos = [];
+const infoArduinos = [];
 
-let date = new Date()
+let date = new Date();
 let min = date.getMinutes();
-// let hour = date.getHours() - 4;
-let hour = date.getHours();
-let fecha = `${hour}:${min}`
+let hour = date.getHours() - 4;
+// let hour = date.getHours();
+let fecha = `${hour}:${min}`;
 
 router.get('/', (req, res) => {
     console.log('Solicitud get');
     count++;
+    let resumenArduino = [];
+
+    listaArduinos.forEach(element => {
+        const registersByArduino = infoArduinos.filter(x => x.id == element);
+        const current = registersByArduino[registersByArduino.length - 1];
+
+        if (current != undefined) {
+            resumenArduino.push({
+                id: element,
+                cantidad_registros: registersByArduino.length,
+                temperatura_actual: current.temperatura,
+                humerdad_actual: current.humedad,
+                registros: registersByArduino
+            })
+        }
+    });
+
+    
     res.json({
         msg: 'get',
         fecha,
-        count,
-        listaArduinos
+        visitas: count,
+        info: resumenArduino
     })
 })
 
 router.post('/', (req, res) => {
-    const {temperatura, humedad} = req.body;
+    const { temperatura, humedad, id } = req.body;
 
     const info = {
+        id,
         temperatura,
         humedad,
         fecha: fecha
     }
 
-    listaArduinos.push(info);
+    if (!listaArduinos.includes(id)) {
+        listaArduinos.push(info.id)
+    }
 
-    console.log("Solicitud post. temperatura:", temperatura, humedad, fecha)
+    infoArduinos.push(info);
+
+    console.log("Solicitud post. temperatura:", info)
     res.json({
         msg: 'post',
         info
